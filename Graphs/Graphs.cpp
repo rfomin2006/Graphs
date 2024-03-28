93cee714d4932da8926689ccd08338849b4bc3e9
rfomin2006 <rfomin2805@gmail.com>
2024-03-28 21:14:26 +0700

    Добавьте файлы проекта.

---
+++ Graphs/Graphs.cpp
@@ -0,0 +1,66 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <list>
+#include <map>
+
+extern "C" {
+    int _sum(int, int);
+    int _sub(int, int);
+    int _mult(int, int);
+    void _print(const char*);
+}
+
+struct Edge;
+
+struct Vertex {
+    std::string name;
+    std::vector<Edge> edges;
+};
+
+struct Edge {
+    int len;
+    Vertex* from = nullptr;
+    Vertex* to = nullptr;
+};
+
+struct Graph {
+    std::vector<Vertex> vertexes;
+    std::vector<Edge> edges;
+
+    void addVertex(std::string name) {
+        Vertex v = Vertex{ name };
+        vertexes.push_back(v);
+    }
+
+    Vertex getOrCreateVertex(std::string name) {
+        for (Vertex v : vertexes) {
+            if (v.name == name) return v;
+        }
+        _print("Not found, created new");
+        return Vertex{ name };
+    }
+
+
+
+};
+
+int main()
+{
+    Graph graph;
+    graph.addVertex("Moscow");
+    graph.addVertex("Piter");
+
+    char* name1 = new char[graph.getOrCreateVertex("Moscow").name.length() + 1];
+    std::strcpy(name1, graph.getOrCreateVertex("Moscow").name.c_str());
+    _print(name1);
+    _print("\n");
+    char* name2 = new char[graph.getOrCreateVertex("Piter").name.length() + 1];
+    std::strcpy(name2, graph.getOrCreateVertex("Piter").name.c_str());
+    _print(name2);
+
+    return 0;
+}
---
+++ Graphs/funcs.asm
@@ -0,0 +1,50 @@
+option casemap:none
+includelib kernel32.lib
+
+extrn WriteFile: proc
+extrn GetStdHandle:proc
+
+.code
+
+public _sum
+public _sub
+public _mult
+public _print
+
+_sum proc
+	mov rax, rcx
+	add rax, rdx
+	ret
+_sum endp
+
+_sub proc
+	mov rax, rcx
+	sub rax, rdx
+	ret
+_sub endp
+
+_mult proc
+	mov rax, rcx
+	imul rax, rdx
+	ret
+_mult endp
+
+_print proc
+	sub rsp, 40
+	mov rdx, rcx
+	mov rcx, -11
+	call GetStdHandle
+	mov rcx, rax
+	mov rax, -1
+	calcLen:
+	inc rax
+	cmp byte ptr [rdx][rax * 1], 0
+	jne calcLen
+	mov r8d, eax
+	xor r9d, r9d
+	mov qword ptr [rsp + 32], 0
+	call WriteFile
+	add rsp, 40
+	ret
+_print endp
+end
